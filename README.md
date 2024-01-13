3da1b779e0ef3b50024fb57884d1bceab36252a9
root <root@Rishivyas>
2024-01-13 16:19:24 +0530

    updated Simple moving average

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Simple_moving_average
-The language used for calculation of Simple moving average is Python. Before running the program make sure Python and Pandas are installed, if not 
-use the command: pip install requirement.txt 
+The language used for calculation of Simple moving average is Python. Before running the program make sure Python and Pandas are installed.
 
-Use the following command to run the program from Simple moving average folder: python3 SMA.py 
+Use the following command to run the program from Simple moving average folder::
 
-After running the program you can see the RELIANCE.csv updated. After update it consists of all the missing SMA values. 
- 
+1. pip install requirement.txt
+2. python3 SMA.py
+
+After running the program you can see the RELIANCE.csv updated. After update it consists of all the missing SMA values.
+
 Along with this, I have attached the OUTPUT.csv which is the output I got.
--- SMA.py
+++ SMA.py
@@ -4,7 +4,7 @@ import numpy as np
 #For reading the contents of csv file
 df = pd.read_csv('RELIANCE.csv')
 
-#Selecting the columns required for SMA calculatiom
+#Selecting the columns required for SMA calculation
 df = df[['Date','Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']]
 
 df['Date']=df['Date']
@@ -17,7 +17,7 @@ df['SMA(Close) 10D'] = df['Close'].rolling(window=10, min_periods=1).mean()
 df['SMA(Adj Close) 10D'] = df['Adj Close'].rolling(window=10, min_periods=1).mean()
 df['SMA(Volume) 10D'] = df['Volume'].rolling(window=10, min_periods=1).mean()
 
-#Since window of the 10, SMA of first 9 elements must be NAN
+#Since the window size is 10, SMA of first 9 elements must be NAN
 df.iloc[:9, 7:] = np.nan
 
 # Assign NaN values for SMA for those input is NAN
@@ -27,7 +27,11 @@ df.loc[df['Low'].isna(), 'SMA(Low) 10D'] = np.nan
 df.loc[df['Close'].isna(), 'SMA(Close) 10D'] = np.nan
 df.loc[df['Adj Close'].isna(), 'SMA(Adj Close) 10D'] = np.nan
 df.loc[df['Volume'].isna(), 'SMA(Volume) 10D'] = np.nan
+
 df=df.round(6)
+
 # For saving the SMA values to a CSV file
 df.to_csv('RELIANCE.csv',index=False)
-print("Updated CSV file")
+
+print("Updated RELIANCE.csv file")
+
