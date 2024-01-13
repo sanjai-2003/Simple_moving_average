d8573b0bd5c3c2308841b6a6659d1c09a01281cd
root <root@Rishivyas>
2024-01-13 15:57:54 +0530

    Simple moving average

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Simple_moving_average
+The language used for calculation Simple moving average is Python. Before running the program make sure Python and Pandas are installed, if not 
+use the command: pip install requirement.txt 
+
+Use the following command to run the program from Simple moving average folder: python3 SMA.py 
+
+After running the program you can see the RELIANCE.csv updated. After update it consists of all the missing SMA values. 
+ 
+Along with this, I have attached the OUTPUT.csv which is the output I got.
---
+++ SMA.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+
+#For reading the contents of csv file
+df = pd.read_csv('RELIANCE.csv')
+
+#Selecting the columns required for SMA calculatiom
+df = df[['Date','Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']]
+
+df['Date']=df['Date']
+
+#Calculating SMA using rolling
+df['SMA(Open) 10D'] = df['Open'].rolling(window=10, min_periods=1).mean()
+df['SMA(High) 10D'] = df['High'].rolling(window=10, min_periods=1).mean()
+df['SMA(Low) 10D'] = df['Low'].rolling(window=10, min_periods=1).mean()
+df['SMA(Close) 10D'] = df['Close'].rolling(window=10, min_periods=1).mean()
+df['SMA(Adj Close) 10D'] = df['Adj Close'].rolling(window=10, min_periods=1).mean()
+df['SMA(Volume) 10D'] = df['Volume'].rolling(window=10, min_periods=1).mean()
+
+#Since window of the 10, SMA of first 9 elements must be NAN
+df.iloc[:9, 7:] = np.nan
+
+# Assign NaN values for SMA for those input is NAN
+df.loc[df['Open'].isna(), 'SMA(Open) 10D'] = np.nan
+df.loc[df['High'].isna(), 'SMA(High) 10D'] = np.nan
+df.loc[df['Low'].isna(), 'SMA(Low) 10D'] = np.nan
+df.loc[df['Close'].isna(), 'SMA(Close) 10D'] = np.nan
+df.loc[df['Adj Close'].isna(), 'SMA(Adj Close) 10D'] = np.nan
+df.loc[df['Volume'].isna(), 'SMA(Volume) 10D'] = np.nan
+df=df.round(6)
+# For saving the SMA values to a CSV file
+df.to_csv('RELIANCE.csv',index=False)
+print("Updated CSV file")
